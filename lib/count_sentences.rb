ea7227d73717ad87d54d249a68766016da813269
jgumerove <josh.gumerove@gmail.com>
2021-02-19 15:32:40 -0500

    commit tree c935262ebe1ad9b986e596b67f08e85419192476

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,25 @@ class String
 
   def sentence?
     
+    self.end_with?(".")
+     
   end
 
   def question?
 
+    self.end_with?("?")
+
   end
 
   def exclamation?
-
+      self.end_with?("!")
   end
 
   def count_sentences
+    
+    arr = self.split(/[!.?]+/)
+    #binding.pry
+    arr.length
 
   end
 end
